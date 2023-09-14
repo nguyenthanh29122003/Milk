1683e325287950db352e65c59d4e56c7805bd495
Thanh Xuan Nguyen <nguyenthanh29122003@gmail.com>
2023-09-14 23:54:31 +0700

    update search login cart

--- script.js
+++ script.js
@@ -1,13 +1,71 @@
-function OnClickLogo()
-{
-    
+// const hidden_forms = document.querySelector(".hidden-forms");
+// const btnSearch = document.querySelector(".header__item header__item--right"); 
+
+// btnSearch.addEventListener("click", ()=>{
+//     hidden_form__search.classList.toggle("search__open");
+// })
+
+
+// const menu = document.querySelector(".hidden-forms");
+// const menuButton = document.querySelector("#btnSearch");
+
+// menuButton.addEventListener("click", () => {
+//     menu.classList.toggle("navbar__open");
+// });
+
+
+function ShowS(){
+    document.querySelector(".hidden-formS").style.opacity = "1";
+    document.querySelector(".hidden-formS").style.transform = "translateX(0)";
+    document.querySelector(".hidden-formS").style. transition = "var(--smooth)";
+
+    document.querySelector(".hidden-formL").style.opacity = "0";
+    document.querySelector(".hidden-formL").style.transform = "translateX(-100%)";
+    document.querySelector(".hidden-formL").style. transition = "var(--smooth)";
+
+    document.querySelector(".hidden-formC").style.opacity = "0";
+    document.querySelector(".hidden-formC").style.transform = "translateX(-100%)";
+    document.querySelector(".hidden-formC").style. transition = "var(--smooth)";
 }
 
-function openSearch() {
-    document.getElementById("myOverlay").style.display = "flex";
-    }
+function ShowL(){
+    document.querySelector(".hidden-formL").style.opacity = "1";
+    document.querySelector(".hidden-formL").style.transform = "translateX(0)";
+    document.querySelector(".hidden-formL").style. transition = "var(--smooth)";
 
-    // Close the full screen search box
-function closeSearch() {
-    document.getElementById("myOverlay").style.display = "none";
-    }
+    document.querySelector(".hidden-formC").style.opacity = "0";
+    document.querySelector(".hidden-formC").style.transform = "translateX(-100%)";
+    document.querySelector(".hidden-formC").style. transition = "var(--smooth)";
+
+    document.querySelector(".hidden-formS").style.opacity = "0";
+    document.querySelector(".hidden-formS").style.transform = "translateX(-100%)";
+    document.querySelector(".hidden-formS").style. transition = "var(--smooth)";
+}
+
+function ShowC(){
+    document.querySelector(".hidden-formC").style.opacity = "1";
+    document.querySelector(".hidden-formC").style.transform = "translateX(0)";
+    document.querySelector(".hidden-formC").style. transition = "var(--smooth)";
+
+    document.querySelector(".hidden-formL").style.opacity = "0";
+    document.querySelector(".hidden-formL").style.transform = "translateX(-100%)";
+    document.querySelector(".hidden-formL").style. transition = "var(--smooth)";
+
+    document.querySelector(".hidden-formS").style.opacity = "0";
+    document.querySelector(".hidden-formS").style.transform = "translateX(-100%)";
+    document.querySelector(".hidden-formS").style. transition = "var(--smooth)";
+}
+
+function CloseF(){
+    document.querySelector(".hidden-formL").style.opacity = "0";
+    document.querySelector(".hidden-formL").style.transform = "translateX(-100%)";
+    document.querySelector(".hidden-formL").style. transition = "var(--smooth)";
+
+    document.querySelector(".hidden-formS").style.opacity = "0";
+    document.querySelector(".hidden-formS").style.transform = "translateX(-100%)";
+    document.querySelector(".hidden-formS").style. transition = "var(--smooth)";
+
+    document.querySelector(".hidden-formC").style.opacity = "0";
+    document.querySelector(".hidden-formC").style.transform = "translateX(-100%)";
+    document.querySelector(".hidden-formC").style. transition = "var(--smooth)";
+}
