407ced19faf3bf4bd9c7392c1141ed4ffd72de5f
Thanh Xuan Nguyen <nguyenthanh29122003@gmail.com>
2023-09-15 23:33:20 +0700

    update search login cart

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// const hidden_forms = document.querySelector(".hidden-forms");
+// const hidden_forms = document.querySelector(".hidden-form__search");
 // const btnSearch = document.querySelector(".header__item header__item--right"); 
 
 // btnSearch.addEventListener("click", ()=>{
@@ -6,7 +6,7 @@
 // })
 
 
-// const menu = document.querySelector(".hidden-forms");
+// const menu = document.querySelector(".hidden-forma");
 // const menuButton = document.querySelector("#btnSearch");
 
 // menuButton.addEventListener("click", () => {
@@ -15,57 +15,101 @@
 
 
 function ShowS(){
-    document.querySelector(".hidden-formS").style.opacity = "1";
-    document.querySelector(".hidden-formS").style.transform = "translateX(0)";
-    document.querySelector(".hidden-formS").style. transition = "var(--smooth)";
+    document.querySelector(".hidden-form__search").style.opacity = "1";
+    document.querySelector(".hidden-form__search").style.transform = "translateX(0)";
+    document.querySelector(".hidden-form__search").style. transition = "var(--smooth)";
 
-    document.querySelector(".hidden-formL").style.opacity = "0";
-    document.querySelector(".hidden-formL").style.transform = "translateX(-100%)";
-    document.querySelector(".hidden-formL").style. transition = "var(--smooth)";
+    document.querySelector(".hidden-form__login").style.opacity = "0";
+    document.querySelector(".hidden-form__login").style.transform = "translateX(-100%)";
+    document.querySelector(".hidden-form__login").style. transition = "var(--smooth)";
 
-    document.querySelector(".hidden-formC").style.opacity = "0";
-    document.querySelector(".hidden-formC").style.transform = "translateX(-100%)";
-    document.querySelector(".hidden-formC").style. transition = "var(--smooth)";
+    document.querySelector(".hidden-form__signup").style.opacity = "0";
+    document.querySelector(".hidden-form__signup").style.transform = "translateX(-100%)";
+    document.querySelector(".hidden-form__signup").style. transition = "var(--smooth)";
+
+    document.querySelector(".hidden-form__recovery").style.opacity = "0";
+    document.querySelector(".hidden-form__recovery").style.transform = "translateX(-100%)";
+    document.querySelector(".hidden-form__recovery").style. transition = "var(--smooth)";
+
+    document.querySelector(".hidden-form__cart").style.opacity = "0";
+    document.querySelector(".hidden-form__cart").style.transform = "translateX(-100%)";
+    document.querySelector(".hidden-form__cart").style. transition = "var(--smooth)";
 }
 
 function ShowL(){
-    document.querySelector(".hidden-formL").style.opacity = "1";
-    document.querySelector(".hidden-formL").style.transform = "translateX(0)";
-    document.querySelector(".hidden-formL").style. transition = "var(--smooth)";
+    document.querySelector(".hidden-form__login").style.opacity = "1";
+    document.querySelector(".hidden-form__login").style.transform = "translateX(0)";
+    document.querySelector(".hidden-form__login").style. transition = "var(--smooth)";
 
-    document.querySelector(".hidden-formC").style.opacity = "0";
-    document.querySelector(".hidden-formC").style.transform = "translateX(-100%)";
-    document.querySelector(".hidden-formC").style. transition = "var(--smooth)";
+    document.querySelector(".hidden-form__signup").style.opacity = "0";
+    document.querySelector(".hidden-form__signup").style.transform = "translateX(-100%)";
+    document.querySelector(".hidden-form__signup").style. transition = "var(--smooth)";
 
-    document.querySelector(".hidden-formS").style.opacity = "0";
-    document.querySelector(".hidden-formS").style.transform = "translateX(-100%)";
-    document.querySelector(".hidden-formS").style. transition = "var(--smooth)";
+    document.querySelector(".hidden-form__search").style.opacity = "0";
+    document.querySelector(".hidden-form__search").style.transform = "translateX(-100%)";
+    document.querySelector(".hidden-form__search").style. transition = "var(--smooth)";
+
+    document.querySelector(".hidden-form__cart").style.opacity = "0";
+    document.querySelector(".hidden-form__cart").style.transform = "translateX(-100%)";
+    document.querySelector(".hidden-form__cart").style. transition = "var(--smooth)";
+
+    document.querySelector(".hidden-form__recovery").style.opacity = "0";
+    document.querySelector(".hidden-form__recovery").style.transform = "translateX(-100%)";
+    document.querySelector(".hidden-form__recovery").style. transition = "var(--smooth)";
 }
 
 function ShowC(){
-    document.querySelector(".hidden-formC").style.opacity = "1";
-    document.querySelector(".hidden-formC").style.transform = "translateX(0)";
-    document.querySelector(".hidden-formC").style. transition = "var(--smooth)";
+    document.querySelector(".hidden-form__cart").style.opacity = "1";
+    document.querySelector(".hidden-form__cart").style.transform = "translateX(0)";
+    document.querySelector(".hidden-form__cart").style. transition = "var(--smooth)";
 
-    document.querySelector(".hidden-formL").style.opacity = "0";
-    document.querySelector(".hidden-formL").style.transform = "translateX(-100%)";
-    document.querySelector(".hidden-formL").style. transition = "var(--smooth)";
+    document.querySelector(".hidden-form__login").style.opacity = "0";
+    document.querySelector(".hidden-form__login").style.transform = "translateX(-100%)";
+    document.querySelector(".hidden-form__login").style. transition = "var(--smooth)";
 
-    document.querySelector(".hidden-formS").style.opacity = "0";
-    document.querySelector(".hidden-formS").style.transform = "translateX(-100%)";
-    document.querySelector(".hidden-formS").style. transition = "var(--smooth)";
+    document.querySelector(".hidden-form__search").style.opacity = "0";
+    document.querySelector(".hidden-form__search").style.transform = "translateX(-100%)";
+    document.querySelector(".hidden-form__search").style. transition = "var(--smooth)";
+
+    document.querySelector(".hidden-form__signup").style.opacity = "0";
+    document.querySelector(".hidden-form__signup").style.transform = "translateX(-100%)";
+    document.querySelector(".hidden-form__signup").style. transition = "var(--smooth)";
+
+    document.querySelector(".hidden-form__recovery").style.opacity = "0";
+    document.querySelector(".hidden-form__recovery").style.transform = "translateX(-100%)";
+    document.querySelector(".hidden-form__recovery").style. transition = "var(--smooth)";
 }
 
 function CloseF(){
-    document.querySelector(".hidden-formL").style.opacity = "0";
-    document.querySelector(".hidden-formL").style.transform = "translateX(-100%)";
-    document.querySelector(".hidden-formL").style. transition = "var(--smooth)";
+    document.querySelector(".hidden-form__login").style.opacity = "0";
+    document.querySelector(".hidden-form__login").style.transform = "translateX(-100%)";
+    document.querySelector(".hidden-form__login").style. transition = "var(--smooth)";
 
-    document.querySelector(".hidden-formS").style.opacity = "0";
-    document.querySelector(".hidden-formS").style.transform = "translateX(-100%)";
-    document.querySelector(".hidden-formS").style. transition = "var(--smooth)";
+    document.querySelector(".hidden-form__search").style.opacity = "0";
+    document.querySelector(".hidden-form__search").style.transform = "translateX(-100%)";
+    document.querySelector(".hidden-form__search").style. transition = "var(--smooth)";
 
-    document.querySelector(".hidden-formC").style.opacity = "0";
-    document.querySelector(".hidden-formC").style.transform = "translateX(-100%)";
-    document.querySelector(".hidden-formC").style. transition = "var(--smooth)";
+    document.querySelector(".hidden-form__signup").style.opacity = "0";
+    document.querySelector(".hidden-form__signup").style.transform = "translateX(-100%)";
+    document.querySelector(".hidden-form__signup").style. transition = "var(--smooth)";
+
+    document.querySelector(".hidden-form__cart").style.opacity = "0";
+    document.querySelector(".hidden-form__cart").style.transform = "translateX(-100%)";
+    document.querySelector(".hidden-form__cart").style. transition = "var(--smooth)";
+
+    document.querySelector(".hidden-form__recovery").style.opacity = "0";
+    document.querySelector(".hidden-form__recovery").style.transform = "translateX(-100%)";
+    document.querySelector(".hidden-form__recovery").style. transition = "var(--smooth)";
+}
+
+function ShowR(){
+    document.querySelector(".hidden-form__signup").style.opacity = "1";
+    document.querySelector(".hidden-form__signup").style.transform = "translateX(0)";
+    document.querySelector(".hidden-form__signup").style. transition = "var(--smooth)";
+}
+
+function ShowReP(){
+    document.querySelector(".hidden-form__recovery").style.opacity = "1";
+    document.querySelector(".hidden-form__recovery").style.transform = "translateX(0)";
+    document.querySelector(".hidden-form__recovery").style. transition = "var(--smooth)";
 }
